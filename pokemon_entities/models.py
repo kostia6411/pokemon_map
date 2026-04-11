de9e90dafd16a8bfc5f242ff63483772641ddd06
kostia6411 <91974940+kostia6411@users.noreply.github.com>
2026-04-11 18:43:13 +0300

    Made the field mandatory

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,19 +1,19 @@
 from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, blank=True, verbose_name='наименование покемона')
+    title = models.CharField(max_length=200, verbose_name='наименование покемона')
     title_en = models.CharField(max_length=200, blank=True, verbose_name='наименование покемона по английски')
     title_jp = models.CharField(max_length=200, blank=True, verbose_name='наименование покемона по японски')
     image = models.ImageField(null=True, blank=True, upload_to='Pokemon_images/', verbose_name='изображение покемона')
     description = models.TextField(max_length=400, blank=True, verbose_name='описание покемона')
-    previous_evolution = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='next_evolution', verbose_name='предыдущия эволюция')
+    previous_evolution = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='next_evolutions', verbose_name='предыдущия эволюция')
 
     def __str__(self):
         return f'{self.title}'
     
 
 class PokePokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='ссылка на покемона', related_name='entities', blank=True, null=True)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='ссылка на покемона', related_name='entities')
     lat = models.FloatField(verbose_name='широта покемона', blank=True, null=True)
     lon = models.FloatField(verbose_name='долгота покемона', blank=True, null=True)
     appeared_at = models.DateTimeField(verbose_name='время появления покемона', blank=True, null=True)
